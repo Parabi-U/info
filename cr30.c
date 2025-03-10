ea2478826108a9d8b8820ccfed44d0d7aed4221a
Parabi-U <paradin0609@gmail.com>
2025-03-10 17:47:52 +0100

    cr30.c

--- cr30.c
+++ cr30.c
@@ -2,19 +2,59 @@
 #include <stdlib.h>
 #include <time.h>
 #define MIN 0
-#define MAX 5
+#define MAX 7
 
 
-int taille(){
-    int tab[MIN + rand()% (MAX -MIN + 1)];
+
+int brut(int tab[]){
+    int taille = MIN + rand() % (MAX - MIN + 1);
     int i;
-    for(i = 0;i < (MAX - MIN);i++){
+    for(i = 0;i < (taille);i++){
         tab[i] = rand() % 2;
     }
-    return (MAX-MIN);
+    tab[taille] = -1;
+    return taille +1;
+}
+
+
+void affiche_brut(int brut[]) {
+    for (int i = 0; brut[i] != -1; i++) {
+        printf("%d ", brut[i]);
+    }
+    printf("-1\n");
+}
+
+void compress_tab(int tab_brut[],int tab_compress[]){
+    int compteur = 0;
+    int i;
+    int j = 0;
+    for(i = 0;i != -1;i++){
+        if(tab_brut[i] == tab_brut[i+1]){
+            compteur++;
+        }
+        else{ if(compteur == 0){
+                tab_compress[j] = tab_brut[i];
+                j++;
+            }
+            else{tab_compress[j] = compteur;
+                tab_compress[j + 1] = tab_brut[i];
+                j++;
+                compteur = 0;
+            }
+        }
+      
+    } 
 }
 
 
 int main(){
+    srand(time(NULL));
+    int tab[MAX +1];
+    int tab_comp[MAX+1];
+    brut(tab);
+    affiche_brut(tab);
+    compress_tab(tab,tab_comp);
+    affiche_brut(tab_comp);
+
     return 0;
 }
